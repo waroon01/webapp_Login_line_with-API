3a8401d6a194b7cee29e38627fb60c6e1930841b
waroon01 <117699848+waroon01@users.noreply.github.com>
2023-09-03 09:49:56 +0700

    Create scriptLinelogin.js

---
+++ แบบไม่ส่งอีเมลล์/scriptLinelogin.js
@@ -0,0 +1,27 @@
+const loginContainer = document.getElementById("loginContainer");
+const loggedInContainer = document.getElementById("loggedInContainer");
+
+const lineLoginButton = document.getElementById("lineLoginButton");
+
+const clientId = "xxxxxxxx"; // channel's client ID
+const clientSecret = 'xxxxxxxxxxxxxxxxxxxxxxxx'; // channel's Secret
+
+
+
+
+lineLoginButton.addEventListener("click", () => {
+  // Redirect 
+  window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${window.location.origin}/pageme.html&state=6789abcde&scope=profile openid email&nonce=01234xyz`;
+  loadstart()
+
+
+});
+
+
+
+function loadstart() {
+  document.querySelector(".loader-container").classList.remove("d-none")
+}
+function loadend() {
+  document.querySelector(".loader-container").classList.add("d-none")
+}
